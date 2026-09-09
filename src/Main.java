048d7dbadf97472295c30e8c79b1d4ca2c1ede3f
Isaac <isaacbassey31@gmail.com>
2026-09-09 17:16:21 +0100

    initial project structure

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import javax.swing.JFrame;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tic-Tac-Toe");
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
