45fdc4096f49433ae0d8d94ba772c5afa30c2104
Isaac <isaacbassey31@gmail.com>
2026-09-09 17:33:59 +0100

    working blank window

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Tic-Tac-Toe");
+        JFrame frame = new JFrame("Tic-Tac-Toe/X and O");
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
